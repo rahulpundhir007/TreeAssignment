e30ba25edaaa7fcbb40f09d73bfe5699b18b1d6a
rahulpundhir007 <your_email@example.com>
2025-09-11 22:31:45 +0530

    Rahul

---
+++ app.js
@@ -0,0 +1,47 @@
+// console.log("Rahul");
+// let h = document.querySelector("h1")
+// let btn = document.querySelector("button")
+// btn.addEventListener("click",()=>{
+// h.style.color = "red"
+// })
+// btn.onclick = function(){
+//     h.style.color = "orange"
+
+// const { createElement } = require("react");
+
+// }
+// let para = document.createElement("p");
+// para.innerText = "Hey I am red";
+// para.style.color = "red";
+// document.querySelector("body").append(para)
+// let h = document.createElement("h3");
+// h.innerText = "I am blue h3!";
+// h.style.color = "blue";
+// document.querySelector("body").append(h)
+// let d  = document.createElement("div");
+// let h1 = document.createElement("h1");
+// let p = document.createElement("p");
+// h1.innerText = "I am inside div";
+// p.innerText = "I am also";
+// d.append(h1);
+// d.append(p);
+// d.style.backgroundColor = "pink";
+// d.style.border = "2px solid black";
+// document.querySelector("body").append(d);
+// d.classList.add("dot")
+// let button = document.createElement("button")
+// let input = document.createElement("input")
+// button.innerText = "Click me"
+// document.querySelector("body").append(button)
+// document.querySelector("body").append(input)
+
+
+
+
+
+
+
+
+
+
+    
